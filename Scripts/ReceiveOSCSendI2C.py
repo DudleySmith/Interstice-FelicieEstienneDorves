947caca27d5a0720b1d368dbb9362fbb12a71669
Dudley smith <account@studio-albert.com>
2020-06-12 15:16:30 +0200

    Async server

--- Scripts/ReceiveOSCSendI2C.py
+++ Scripts/ReceiveOSCSendI2C.py
@@ -4,7 +4,9 @@ import threading
 
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.osc_server import AsyncIOOSCUDPServer
 
+import asyncio
 import I2CControl
 
 #setup Logging
@@ -63,13 +65,28 @@ try:
 #     ip = "192.168.0.101"
 #     port = 9000
 #     server = BlockingOSCUDPServer((ip, port), dispatcher)
-# 
+#
 #     logging.info('Launch server OSC ' + ip + ":" + str(port))
-# 
+#
 #     print("Server started ++")
 #     server.serve_forever()  # Blocks forever
 
+    async def loop():
+        """Example main loop that only runs for 10 iterations before finishing"""
+        for i in range(10):
+            print(f"Loop {i}")
+            await asyncio.sleep(1)
 
+
+    async def init_main():
+        server = AsyncIOOSCUDPServer((ip, port), dispatcher, asyncio.get_event_loop())
+        transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
+
+        await loop()  # Enter main loop of program
+
+        transport.close()  # Clean up serve endpoint
+
+    asyncio.run(init_main())
 
 except OSError as e:
     print("Wrong address ??? : {0}".format(e))
